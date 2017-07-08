e0644b803b453e16c30c9d95e6a17414c9db9e12
venkat5432 <coolguyvenkatraman@gmail.com>
2017-07-08 20:25:58 +0530

    after tested with all browsers

--- src/test/java/com/project/php_travels/Test.java
+++ src/test/java/com/project/php_travels/Test.java
@@ -1,7 +1,10 @@
 package com.project.php_travels;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Test {
 
@@ -10,8 +13,29 @@ public class Test {
 	@org.testng.annotations.Test
 	public void test()
 	{
-		System.setProperty("webdriver.gecko.driver","C:\\Custom_Softwares\\geckodriver-v0.17.0-win32\\geckodriver.exe");
+		/*System.setProperty("webdriver.gecko.driver","C:\\Custom_Softwares\\geckodriver-v0.17.0-win32\\geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
 		driver.get("https://www.google.co.in");
+		*/
+		
+		/*
+		System.setProperty("webdriver.chrome.driver", "C:\\Custom_Softwares\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.yahoo.co.in");
+		
+		*/
+
+/*		System.setProperty("webdriver.ie.driver", "C:\\Custom_Softwares\\IEDriverServer_Win32_3.4.0\\IEDriverServer.exe");
+		WebDriver driver=new InternetExplorerDriver();
+		driver.get("https://www.google.co.in");
+		
+*/		
+
+/*
+		System.setProperty("webdriver.edge.driver", "C:\\Custom_Softwares\\Edgedriver\\MicrosoftWebDriver.exe");
+		WebDriver driver=new EdgeDriver();
+		driver.get("https://www.google.co.in");
+*/
+
 	}
 }
